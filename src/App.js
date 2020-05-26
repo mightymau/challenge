2861ad3fd32147ee4b9e7b6401f8ea93027cdb6a
Lisandro Molina <lisandromolina@Lisandros-MacBook-Pro.local>
2020-05-26 10:48:28 +0800

    Main layout

--- src/App.js
+++ src/App.js
@@ -1,56 +1,52 @@
 import React from 'react';
 import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <div className="sideBar">
+        <img src="./app_logo.png" className="App-logo" alt="logo" />
+        <ul>
+          <li className="selected">Profile</li>
+          <li>Help</li>
+        </ul>
+      </div>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
+        <h1>Profile</h1>
       </header>
+      <main>
+        <div className="profile-info">
+          <img className="profile-pic" src="./profile.png" alt="Profile Pic" />
+          <a href="#">Edit Profile</a>
+          <label for="full-name">Full Name</label>
+          <p id="full-name">super admin</p>
+          <label for="email">Email</label>
+          <p id="full-name">super@admin.com</p>
+          <div>
+            <a href="#">Change Password</a>
+          </div>
+          <div>
+            <label for="language">Select App Language</label>
+            <select name="language" id="language">
+              <option>English</option>
+            </select>
+          </div>
+          <div>
+            <a href="#">Logout</a>
+            <span>
+              <input type="checkbox" id="logoutAllDevices" name="logoutAllDevices" />
+                <label for="logoutAllDevices">Logout All Devices</label>
+
+            </span>
+          </div>
+        </div>
+        <div className="recent-activity">
+          <h2>Recent Activity</h2>
+        </div>
+
+      </main>
+
     </div>
   );
 }
